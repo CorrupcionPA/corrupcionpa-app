c921a404c783dcfbb19dd69e8030dd054aa01fd3
Jesus Castaneda <jcs.98126@gmail.com>
2023-11-23 22:40:35 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NzPageHeaderModule, NzBreadCrumbModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
