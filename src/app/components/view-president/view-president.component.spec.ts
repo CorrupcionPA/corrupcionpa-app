f2b335e6016856b8a3c6c6df1deb34ca9a32d2ce
Jesus Castaneda <jcs.98126@gmail.com>
2023-11-27 23:54:24 -0500

    Applied prettier formatting (#12)
    * Fixed breadcrumb behaviour
    
    * apply prettier formatting

--- src/app/components/view-president/view-president.component.spec.ts
+++ src/app/components/view-president/view-president.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { ViewPresidentComponent } from './view-president.component';
+import { ViewPresidentComponent } from './view-president.component'
 
 describe('ViewPresidentComponent', () => {
-  let component: ViewPresidentComponent;
-  let fixture: ComponentFixture<ViewPresidentComponent>;
+  let component: ViewPresidentComponent
+  let fixture: ComponentFixture<ViewPresidentComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ViewPresidentComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(ViewPresidentComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      imports: [ViewPresidentComponent],
+    }).compileComponents()
+
+    fixture = TestBed.createComponent(ViewPresidentComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
