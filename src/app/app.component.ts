8f7ecc110cc253e19d2a66ade8dade3870848c92
Jesus Castaneda <jcs.98126@gmail.com>
2023-11-27 23:46:44 -0500

    Fixed breadcrumb behaviour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { RouterOutlet } from '@angular/router'
+import { RouterOutlet, RouterModule } from '@angular/router'
 import { NzPageHeaderModule } from 'ng-zorro-antd/page-header'
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NzPageHeaderModule, NzBreadCrumbModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterModule,
+    NzPageHeaderModule,
+    NzBreadCrumbModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/components/view-president/view-president.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'app-view-president',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './view-president.component.html',
+  styleUrl: './view-president.component.scss',
+})
+export class ViewPresidentComponent {}
