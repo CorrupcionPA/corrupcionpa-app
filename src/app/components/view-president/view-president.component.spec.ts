74361cc94d54a369da88adfdb104406c5a79f931
Jesus Castaneda <jcs.98126@gmail.com>
2023-11-27 23:50:16 -0500

    Fixed breadcrumb behaviour (#11)

---
+++ src/app/components/view-president/view-president.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ViewPresidentComponent } from './view-president.component';
+
+describe('ViewPresidentComponent', () => {
+  let component: ViewPresidentComponent;
+  let fixture: ComponentFixture<ViewPresidentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ViewPresidentComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ViewPresidentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
