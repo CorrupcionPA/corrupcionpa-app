6aaa6381b7a2c676a9ada1416e1bc8c995489e02
Jesus Castaneda <jcs.98126@gmail.com>
2023-11-28 00:08:25 -0500

    Created types

---
+++ src/app/types/Partido.ts
@@ -0,0 +1,8 @@
+export type Partido = {
+  id: string
+  siglas: string
+  nombrePartido: string
+  nombreFundador: string
+  cantidadMiembros: number
+  fechaCreacion: Date
+}
---
+++ src/app/types/Politico.ts
@@ -0,0 +1,7 @@
+export type Politico = {
+  id: string
+  idPartido: string
+  nombre: string
+  inicioPeriodo: Date
+  finPeriodo: Date
+}
