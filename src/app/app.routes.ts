20d8e9b5a5b779ab53e87759ad50cf8f8848b3a9
Carlos Bazan <carlosbazanguerra@gmail.com>
2023-11-29 22:12:40 -0500

    Add correct typing for president

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router'
 import { HomeComponent } from './components/home/home.component'
 import { ViewPresidentComponent } from './components/view-president/view-president.component'
+import { ChoosePresidentComponent } from './components/choose-president/choose-president.component'
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent, data: { breadcrumb: 'Home' } },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'choose-president',
+    component: ChoosePresidentComponent,
+    data: { breadcrumb: 'Choose President' },
+  },
   {
     path: 'president/:id',
     component: ViewPresidentComponent,
