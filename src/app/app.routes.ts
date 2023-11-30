1d691e36bb3d4ad4035cbc2dce74aeb8cc27815b
Carlos Bazan <carlosbazanguerra@gmail.com>
2023-11-29 20:49:12 -0500

    creating of choose president

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,4 @@
 import { Routes } from '@angular/router'
 import { HomeComponent } from './components/home/home.component'
-import { ViewPresidentComponent } from './components/view-president/view-president.component'
 
-export const routes: Routes = [
-  { path: 'home', component: HomeComponent, data: { breadcrumb: 'Home' } },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  {
-    path: 'president/:id',
-    component: ViewPresidentComponent,
-    data: { breadcrumb: ':id' },
-  },
-]
+export const routes: Routes = [{ path: '', component: HomeComponent }]
