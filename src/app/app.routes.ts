509d65f4d2cbcb840e55d5c4228563cf6583b0b7
Carlos Bazan <carlosbazanguerra@gmail.com>
2023-11-29 19:26:06 -0500

    ruta de choose.president agregada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router'
 import { HomeComponent } from './components/home/home.component'
-
-export const routes: Routes = [{ path: '', component: HomeComponent }]
+import { ChoosePresidentComponent } from './components/choose-president/choose-president.component'
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'choose-president', component: ChoosePresidentComponent },
+]
